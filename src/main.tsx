954baa03e3fe9bf20228a345350806cf4063cc83
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-07-13 20:58:01 -0300

    general updates and cleaning

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,4 @@ import App from "./App";
 
 import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
